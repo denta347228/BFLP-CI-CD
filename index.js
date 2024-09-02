db4e0e504f88e78914b11c5585734ecdbfc3b7b6
Denta_Register_Github <dentamustofa625@gmail.com>
2024-09-02 10:24:55 +0700

    bflp-it

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "BFLP-IT" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Martabak Manis" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
